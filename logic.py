1fcc66552590061b78ddb5f8ffefed01f45d2e31
GloubyBoulga <romain.gascoin@gmail.com>
2021-04-01 13:13:11 +0000

    rewrote unsaved code

--- logic.py
+++ logic.py
@@ -1,26 +1,6 @@
 import sqlite3
 import common
 
-conn = sqlite3.connect(grid)
-
-def is_valid(grid):
-	"""
-	TODO
-	Takes a SudokuGrid object in and returns True if it is a valid sudoku grid. Else, returns False.
-	TODO
-	"""
-	pass
-
-
-def create_full_grid():
-	"""
-	TODO
-	Returns a solved sudoku grid created randomly
-	TODO
-	"""
-	pass
-
-
 def grid_to_text(grid):
 	"""
 	compile la grille sous forme de texte
@@ -28,11 +8,19 @@ def grid_to_text(grid):
 	un & signifie un espace
 	"""
 	text = ""
-	for x in range(0,10):
-		for y in range(0,10):
-			text += str(grid.get_number((x,y)))
+	for x in range(9):
+		for y in range(9):
+            if grid.get_number((x,y)) == 0 :
+                text += "&"
+            else :
+			    text += str(grid.get_number((x,y)))
 			
-
+def text_to_grid(text):
+    """
+    Prends le texte simplifié de la grille
+    et renvoie l'objet
+    """
+    pass
 
 def rows(grid):
 	"""
