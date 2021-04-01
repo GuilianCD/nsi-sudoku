2d8fd6966b83fb89abaaa9bdf9645a6d0ce1138e
GloubyBoulga <romain.gascoin@gmail.com>
2021-04-01 13:56:43 +0000

    Wrote text to grid

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 import sqlite3
 import common
+from math import sqrt
 
 def grid_to_text(grid):
 	"""
@@ -7,9 +8,9 @@ def grid_to_text(grid):
 	ligne par ligne
 	un & signifie un espace
 	"""
-	text = ""
-	for x in range(9):
-		for y in range(9):
+	text = f"{grid.size}:" #le premier élément du texte est la taille de la grille
+	for x in range(grid.size):
+		for y in range(grid.size):
             if grid.get_number((x,y)) == 0 :
                 text += "&"
             else :
@@ -20,7 +21,21 @@ def text_to_grid(text):
     Prends le texte simplifié de la grille
     et renvoie l'objet
     """
-    pass
+    
+    
+    grid_size = sqrt(len(text))
+    grid = []
+    for x in range(grid_size):
+	    row = []
+	    for y in range(grid_size):
+            val = text[x * grid_size + y]
+		    row.append(val if val != '&' else None)
+	    grid.append(row)
+
+    sudokugrid = common.SudokuGrid(grid_size)
+    sudokugrid.grid = grid #L'objet sudokugrid correspond à la grille lue
+
+    return sudokugrid
 
 def rows(grid):
 	"""
